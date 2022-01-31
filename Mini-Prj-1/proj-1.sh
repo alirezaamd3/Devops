99e3a6db4e314c96bdcebdd6c6752abd547c6251
Alireza Ahmadi <a.ahmadi@sinacomsys.com>
2022-01-31 19:17:15 +0330

    Minor changes

--- Mini-Prj-1/proj-1.sh
+++ Mini-Prj-1/proj-1.sh
@@ -12,8 +12,12 @@ echo "Installing Redis..."
 apt install -y redis
 
 echo "Installing .NET ..."
-apt-get install -y apt-transport-https && \
-snap install dotnet-sdk --classic
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt-get install -y apt-transport-https
+apt-get install -y dotnet-sdk-3.1
+apt-get install -y aspnetcore-runtime-3.1
 
 echo "Installing NPM ..."
 apt install -y npm
@@ -39,7 +43,7 @@ systemctl restart postgres.service
 
 echo "Configuring Redis ..."
 cp /opt/devops/Mini-Prj-1/config/redis.conf /etc/redis/redis.conf
-systemctl restart postgres.service
+systemctl restart redis-server.service
 
 echo "Configuring Nginx ..."
 cp /opt/devops/Mini-Prj-1/config/default /etc/nginx/sites-enabled/
@@ -57,6 +61,7 @@ cd /opt/devops/voting-system/worker-master
 dotnet publish -c Release -o ./out Worker.csproj
 
 echo "Installing services ... "
+chmod +x /opt/devops/Mini-Prj-1/runner/*.sh
 cp /opt/devops/Mini-Prj-1/service/* /etc/systemd/system/
 systemctl daemon-reload 
 systemctl status vote.service
